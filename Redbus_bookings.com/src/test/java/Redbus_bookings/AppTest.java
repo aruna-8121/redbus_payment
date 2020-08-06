08ac86fb3e0b4adec592809c8c7898fa1c0a1793
Raghav <aruna-8121>
2020-08-06 21:29:48 +0530

    jarrr file

---
+++ Redbus_bookings.com/src/test/java/Redbus_bookings/AppTest.java
@@ -0,0 +1,20 @@
+package Redbus_bookings;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
